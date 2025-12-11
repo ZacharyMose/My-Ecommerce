46319649e692a651a8f327a399c9ce59ba9cfaaa
Zachary Mose <mosezachary198@gmail.com>
2025-12-11 14:18:07 +0300

    products microservice

---
+++ product-service/src/test/java/com/mose/ecommercebackend/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mose.ecommercebackend.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
